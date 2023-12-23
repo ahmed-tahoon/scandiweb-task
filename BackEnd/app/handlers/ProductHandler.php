e5016e864d500b2f1d66b44e67d5c75328e45c71
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2023-12-23 13:13:40 +0200

    Update ProductHandler.php

--- BackEnd/app/handlers/ProductHandler.php
+++ BackEnd/app/handlers/ProductHandler.php
@@ -18,41 +18,34 @@ class ProductHandler
     public function handleRequest($requestData)
     {
         try {
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            
-                if (isset($requestData['action'])) {
-                    if ($requestData['action'] === 'createProduct') {
-                        $product = $this->productService->createProduct($requestData);
-                        $this->productService->saveProduct($product);
-                        ResponseHelper::sendSuccessResponse(['product' => $product], 'Product added successfully');
-                    } elseif ($requestData['action'] === 'deleteMultipleProducts') {
-                        // Logic for deleting multiple products
-                        
-                        $productIds = $requestData['productIds'] ?? [];
-                        $productIdsArray = explode(',', $productIds);
-                        if (!empty($productIdsArray)) {
-                            $deleted = $this->productService->deleteMultiProducts($productIdsArray);
-            
-                            if ($deleted) {
-                                ResponseHelper::sendSuccessResponse([], 'Products deleted successfully');
-                            } else {
-                                ResponseHelper::sendErrorResponse('Products not found', 404);
-                            }
-                        } else {
-                            ResponseHelper::sendErrorResponse('Product IDs are required for deletion', 400);
-                        }
+            $json_data = file_get_contents('php://input');
+            $requestData = json_decode($json_data, true);
+
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/api/products') {
+
+
+
+                $product = $this->productService->createProduct($requestData);
+                $this->productService->saveProduct($product);
+                ResponseHelper::sendSuccessResponse(['product' => $product], 'Product added successfully');
+            } else if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/api/products/delete') {
+                $productIds = $requestData['productIds'] ?? "";
+                $productIdsArray = explode(',', $productIds);
+                if (!empty($productIdsArray)) {
+                    $deleted = $this->productService->deleteMultiProducts($productIdsArray);
+
+                    if ($deleted) {
+                        ResponseHelper::sendSuccessResponse([], 'Products deleted successfully');
                     } else {
-                        // Handle other actions if needed
-                        ResponseHelper::sendErrorResponse('Invalid action', 400);
+                        ResponseHelper::sendErrorResponse('Products not found', 404);
                     }
                 } else {
-                    // Handle other POST request scenarios if action parameter is not present
-                    ResponseHelper::sendErrorResponse('Action parameter missing', 400);
+                    ResponseHelper::sendErrorResponse('Product IDs are required for deletion', 400);
                 }
-            } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            } elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === '/api/products') {
                 $products = $this->productService->getAllProducts();
                 ResponseHelper::sendSuccessResponse(['products' => $products], 'Products retrieved successfully');
-            }  else {
+            } else {
                 ResponseHelper::sendErrorResponse('Invalid request method', 400);
             }
         } catch (Exception $e) {
